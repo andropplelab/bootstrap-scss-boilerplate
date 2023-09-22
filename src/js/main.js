a0ef69a29a616977702e1e01ef73ace96c7d05a0
andropplelab <mudssar.pk@gmail.com>
2023-09-22 07:19:33 -0700

    intent release

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+const androppleApp = function () {
+    // write your code here ...
+    
+}()
+window.addEventListener("DOMContentLoaded", (event) => {
+    androppleApp.init();
+});
+
+window.androppleApp = androppleApp
+window.bootstrap = bootstrap
+
+
